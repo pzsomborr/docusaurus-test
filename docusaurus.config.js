c00078fc7a6082320b07cb8e172750e219f4819b
pzsomborr <zsombor.palfi@gmail.com>
2024-08-29 22:36:12 +0200

    Website content update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,8 +25,8 @@ const config = {
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: 'ignore',
+  onBrokenMarkdownLinks: 'ignore',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
